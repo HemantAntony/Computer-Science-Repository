93ecce2b98f38fb33e1c289664630d6c000400e8
HemantAntony <88136054+HemantAntony@users.noreply.github.com>
2021-08-05 12:35:42 +0530

    Creating Test.py
    Remove the Comments and type in some code

---
+++ Test.py
@@ -0,0 +1,18 @@
+def Alan():
+    #Alan complete this
+  
+def Albin():
+    #Albin complete this
+
+def Hemant():
+	print("I am Hemant")
+    print("How are you?")
+
+
+name = input("Hello, Which function do you want to call?")
+if name == "Alan":
+    Alan()
+elif name == "Albin":
+    Albin()
+else:
+    Hemant()
