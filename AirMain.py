ab400d4f9b6fb9f51c03bd526c43784d13843c32
HemantAntony <88136054+HemantAntony@users.noreply.github.com>
2021-08-08 15:09:06 +0530

    Update AirMain.py

--- AirMain.py
+++ AirMain.py
@@ -1,3 +1,10 @@
 print("Hello there! Which mode do you want?")
 print("1. Client Mode (c)")
 print("2. Admin Mode (a)")
+answer = input()
+if answer == 'c':
+  print("Client")
+elif answer == 'a':
+  print("Admin")
+else:
+  print("Invalid Input")
